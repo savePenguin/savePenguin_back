f779b09435bfcd0d8d2ef33bbae53ce9d99f6ec3
hwikyungkim-1215 <buc06137@naver.com>
2022-01-29 14:46:50 +0900

    init

---
+++ src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,34 @@
+package com.cos.photogramstart.config.auth;
+//로그인 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.domain.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service // IoC
+public class PrincipalDetailsService implements UserDetailsService{
+
+	private final UserRepository userRepository;  //userid DB에 있는지 확인하기 위해 
+	
+	
+	// 1. 패스워드는 알아서 체킹하니까 신경쓸 필요 없다.
+	// 2. 리턴이 잘되면 자동으로 UserDetails 타입을 세션으로 만든다.
+	@Override
+	public UserDetails loadUserByUsername(String useid) throws UsernameNotFoundException {
+		
+		User userEntity = userRepository.findByUsername(useid);
+		
+		if(userEntity == null) {
+			return null;
+		}else {
+			return new PrincipalDetails(userEntity);
+		}
+	}
+
+}
---
+++ src/main/java/com/cos/photogramstart/web/dto/user/UserUpdateDto.java
@@ -0,0 +1,26 @@
+package com.cos.photogramstart.web.dto.user;
+
+import javax.validation.constraints.NotBlank;
+
+import com.cos.photogramstart.domain.user.User;
+
+import lombok.Data;
+
+// 회원수정에서 뭐 바꿀 수 있는지 확인하
+@Data
+public class UserUpdateDto { //회원 수
+	@NotBlank
+	private String username; // 필수
+	@NotBlank
+	private String password; // 필수
+	private String useremail;
+	
+	// 조금 위함함. 코드 수정이 필요할 예정
+	public User toEntity() {
+		return User.builder()
+				.username(username) // 이름을 기재 안했으면 문제!! Validation 체크
+				.password(password) // 패스워드를 기재 안했으면 문제!! Validation 체크
+				.useremail(useremail)
+				.build();
+	}
+}
